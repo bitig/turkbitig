45f00ecbbdfd3f2d5e62f0d25bf38a5a78208299
bitig <turkanitlari@gmail.com>
2023-12-28 22:10:16 +0800

    none

--- theme.js
+++ theme.js
@@ -1,22 +1,61 @@
-    function setTheme(theme) {
-      const themeStyle = document.getElementById('theme-style');
-      themeStyle.href = '../' + theme + '.css';
-      localStorage.setItem('theme', theme);
-    }
+// Get the theme toggle button element
+const themeToggle = document.querySelector('#theme-toggle');
 
-    function toggleTheme() {
-      const currentTheme = localStorage.getItem('theme');
-      const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-      setTheme(newTheme);
-    }
+// Get the <link> element for the stylesheet
+const styleSheet = document.querySelector('#theme-style');
 
-    window.addEventListener('DOMContentLoaded', function() {
-      const storedTheme = localStorage.getItem('theme');
-      if (storedTheme) {
-        setTheme(storedTheme);
-      }
+// Store the current theme preference in a variable
+let currentTheme = localStorage.getItem('themePreference');
 
-      const themeToggleBtn = document.getElementById('theme-toggle');
-      themeToggleBtn.addEventListener('click', toggleTheme);
-    });
+// Check if a theme preference is stored in local storage
+if (!currentTheme) {
+  // Set the default theme preference to light
+  currentTheme = 'light';
+}
 
+// Apply the initial theme preference
+setTheme(currentTheme);
+
+// Update the theme toggle button text
+updateThemeToggleText(currentTheme);
+
+// Listen for click events on the theme toggle button
+themeToggle.addEventListener('click', () => {
+  // Toggle the theme between light and dark
+  toggleTheme();
+});
+
+// Function to toggle the theme
+function toggleTheme() {
+  // Check the current theme and switch to the opposite theme
+  if (currentTheme === 'light') {
+    setTheme('dark');
+  } else {
+    setTheme('light');
+  }
+}
+
+// Function to set the theme
+function setTheme(theme) {
+  // Get the base URL of the current page
+  const baseUrl = window.location.origin + window.location.pathname;
+
+  // Update the stylesheet href based on the theme
+  styleSheet.setAttribute('href', `../${theme}.css`);
+
+
+  // Update the current theme
+  currentTheme = theme;
+
+  // Update the theme toggle button text
+  updateThemeToggleText(theme);
+
+  // Store the theme preference in local storage
+  localStorage.setItem('themePreference', theme);
+}
+
+// Function to update the theme toggle button text
+function updateThemeToggleText(theme) {
+  const switchText = theme === 'light' ? 'KARA' : ' AK ';
+  themeToggle.textContent = `${switchText}`;
+}
