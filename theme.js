c59962504a3bf331fc7a4f6ecd559edf70743683
bitig <turkanitlari@gmail.com>
2023-12-29 17:06:05 +0800

    none

--- theme.js
+++ theme.js
@@ -59,3 +59,18 @@ function updateThemeToggleText(theme) {
   const switchText = theme === 'light' ? 'KARA' : ' AK ';
   themeToggle.textContent = `${switchText}`;
 }
+
+
+function showTheme() {
+  // Check Local Storage then apply dark theme if it's active
+}
+
+function showContent() {
+  document.body.style.visibility = 'visible';
+  document.body.style.opacity = 1;
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+  showTheme();
+  showContent();
+});
