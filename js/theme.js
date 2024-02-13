48085b55c3b3a31968123a2cddccfa0015efdfde
bitig <turkanitlari@gmail.com>
2024-02-13 20:31:41 +0800

    none

--- js/theme.js
+++ js/theme.js
@@ -94,7 +94,7 @@ if (savedPreference) {
 
 // Increase font size on button click
 increaseBtn.addEventListener("click", function() {
-  if (fSize < 23) {
+  if (fSize < 22) {
     fSize += 2;
     paragraph.style.fontSize = fSize + "px";
     saveFontSizePreference(fSize);
