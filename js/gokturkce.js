a36ef30984b388be03938a4ceeecadf9b7dfc4ce
bitig <turkanitlari@gmail.com>
2024-05-18 15:09:46 +0800

    none

--- js/gokturkce.js
+++ js/gokturkce.js
@@ -3,6 +3,8 @@
 var vowelsMap={'a':'𐰀','e':'𐰀','ı':'𐰃','i':'𐰃','o':'𐰆','u':'𐰆','ö':'𐰇','ü':'𐰇',};
 var consonantsMap={'b':['𐰉','𐰋'],'d':['𐰑','𐰓'],'g':['𐰍','𐰏'],'k':['𐰴','𐰚'],'l':['𐰞','𐰠'],'n':['𐰣','𐰤'],'r':['𐰺','𐰼'],'s':['𐰽','𐰾'],'t':['𐱃','𐱅'],'y':['𐰖','𐰘'],'ç':['𐰲','𐰲'],'ñ':['𐰭','𐰭'],'ŋ':['𐰭','𐰭'],'m':['𐰢','𐰢'],'p':['𐰯','𐰯'],'ş':['𐱁','𐱁'],'z':['𐰔','𐰔'],'c':['𐰲','𐰲'],'f':['𐰯','𐰯'],'ğ':['𐰍','𐰏'],'h':['𐰴','𐰚'],'j':['𐱁','𐱁'],'v':['𐰉','𐰋'],' ':['  ','  '],};
 var doublesMap={'ng':'𐰭','nç':'𐰨','nd':'𐰦','nt':'𐰦','ny':'𐰪','ok':'𐰸','uk':'𐰸','ök':'𐰜','ük':'𐰜','iç':'𐰱','ık':'𐰶','ld':'𐰡','lt':'𐰡',};
+var vowels = ['a', 'ı', 'o', 'u', 'e', 'i', 'ö', 'ü'];
+var consonants = ['b', 'c', 'ç', 'd', 'f', 'g', 'ğ', 'h', 'j', 'k', 'l', 'n', 'm', 'p', 'r', 's', 'ş', 't', 'v', 'y', 'z'];
 var backVowels = ['a', 'ı', 'o', 'u'];
 var frontVowels = ['e', 'i', 'ö', 'ü'];
 
@@ -48,14 +50,35 @@ function processInputText(inputText) {
         var mappedCharacters = consonantsMap[currentChar];
 
         if (
-          (i < inputText.length - 1 && isFrontVowel(inputText[i + 1]))
+          (i < inputText.length - 1 
+            && isFrontVowel(inputText[i + 1])
+            && !isBackVowel(inputText[i - 1])
+            )
           || (
-            (i > 0 && !isBackVowel(inputText[i + 1])) &&
-            (i > 0 && isFrontVowel(previousChar)) 
+            (i > 0 
+              && isBackVowel(previousChar) 
+              && isFrontVowel(inputText[i + 1])
+              && !isConsonant(inputText[i + 2])
+             )
           )
           || (
-            (i > 0 && !isBackVowel(inputText[i + 1])) &&
-            (i > 0 && isFrontVowel(inputText[i - 2]))
+            (i > 0 
+              && isFrontVowel(previousChar) 
+              && isBackVowel(inputText[i + 1])
+              && isConsonant(inputText[i + 2])
+             )
+          )
+          || (
+            (i > 0 
+              && isFrontVowel(previousChar) 
+              && !isBackVowel(inputText[i + 1])
+             )
+          )
+          || (
+            (i > 0 
+              && isFrontVowel(inputText[i - 2])
+              && !isBackVowel(inputText[i + 1])
+              )
           )
         ) {
           processedText += mappedCharacters[1];
@@ -80,4 +103,12 @@ function isFrontVowel(char) {
 
 function isBackVowel(char) {
   return backVowels.includes(char);
+}
+
+function isVowel(char) {
+  return vowels.includes(char);
+}
+
+function isConsonant(char) {
+  return consonants.includes(char);
 }
