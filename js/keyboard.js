ce6f80edb0ffa6d17a2f4555695b13e782b4ddcf
ismet <turkanitlari@gmail.com>
2024-09-26 14:27:04 +0800

    none

--- js/keyboard.js
+++ js/keyboard.js
@@ -8,7 +8,6 @@ var fontSize = 220;
 var lineHeight = 0.96;
 var paddingTop = 10;
 var paddingRight = 10;
-//var canvasColor = "white"; // initialize canvas background color to white
 var debounceTimer;
 
 var minusButton = document.getElementById("minus");
@@ -30,59 +29,54 @@ latin.addEventListener("input", () => {
   }, 20);
 });
 
-// Add additional event listener to gtext element that triggers updateCanvas function
 latin.addEventListener("change", () => {
   updateCanvas();
 });
 
 minusButton.addEventListener("click", () => {
-    if (fontSize > 20) { // Check if font size is greater than 20
-      fontSize -= 10;
+  if (fontSize > 20) {
+    fontSize -= 10;
+    updateCanvas();
+  }
+});
+
+plusButton.addEventListener("click", () => {
+  if (fontSize < 800) {
+    fontSize += 10;
+    updateCanvas();
+  }
+});
+
+colorRadios.forEach(radio => {
+  radio.addEventListener('change', () => {
+    updateCanvas();
+  });
+});
+
+fontRadios.forEach(radio => {
+  radio.addEventListener('change', () => {
+    updateCanvas();
+  });
+});
+
+bgColorRadios.forEach(radio => {
+  radio.addEventListener('change', (event) => {
+    if (event.target.value === 'transparent') {
+      canvas.style.backgroundColor = 'transparent';
+    } else {
+      canvasColor = event.target.value;
       updateCanvas();
     }
   });
-
-plusButton.addEventListener("click", () => {
-    if (fontSize < 800) {
-        fontSize += 10;
-        updateCanvas();
-    }
 });
 
-// Handle color radio button changes
-for (var i = 0; i < colorRadios.length; i++) {
-    colorRadios[i].addEventListener('change', () => {
-        updateCanvas();
-    });
-}
-
-// Handle font radio button changes
-for (var i = 0; i < fontRadios.length; i++) {
-    fontRadios[i].addEventListener('change', () => {
-        updateCanvas();
-    });
-}
-
-// Handle background color radio button changes
-for (var i = 0; i < bgColorRadios.length; i++) {
-    bgColorRadios[i].addEventListener('change', (event) => {
-        if (event.target.value === 'transparent') {
-            canvas.style.backgroundColor = 'transparent';
-        } else {
-            canvasColor = event.target.value;
-            updateCanvas();
-        }
-    });
-}
-
-// Add event listener for arrow key presses
 document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") { // Increase font size if up arrow key pressed
+  if (event.key === "ArrowLeft") {
     if (fontSize < 2000) {
       fontSize += 6;
       updateCanvas();
     }
-  } else if (event.key === "ArrowRight") { // Decrease font size if down arrow key pressed
+  } else if (event.key === "ArrowRight") {
     if (fontSize > 18) {
       fontSize -= 6;
       updateCanvas();
@@ -91,13 +85,9 @@ document.addEventListener("keydown", (event) => {
 });
 
 function updateCanvas(force = false) {
-  // Clear canvas
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Get input text from hidden textarea
   var inputText = gtext.value;
-
-  // Split input text into separate lines
   var maxWidth = canvas.width - paddingRight;
   var lines = [];
   var words = inputText.split(" ");
@@ -115,58 +105,19 @@ function updateCanvas(force = false) {
   }
   lines.push(currentLine);
 
-  // Calculate canvas height based on number of lines
   var textHeight = lines.length * fontSize * lineHeight;
   var canvasHeight = textHeight + paddingTop * 2;
   canvas.height = canvasHeight;
 
-  // Set text color based on radio button selection
   var selectedColor = document.querySelector(
     'input[name="colorRadio"]:checked'
   ).value;
   ctx.fillStyle = selectedColor;
 
-  // Set font family based on radio button selection
   var selectedFont = document.querySelector(
     'input[name="fontRadio"]:checked'
   ).value;
-  if (selectedFont === "damgab") {
-    ctx.font = fontSize + "px damgab";
-  } else if (selectedFont === "damgabb") {
-    ctx.font = fontSize + "px damgabb";
-  } else if (selectedFont === "tbg") {
-    ctx.font = fontSize + "px tbg";
-  } else if (selectedFont === "cizgi") {
-    ctx.font = fontSize + "px cizgi";
-  } else if (selectedFont === "tatarbodun") {
-    ctx.font = fontSize + "px tatarbodun";
-  } else if (selectedFont === "tonyukuk") {
-    ctx.font = fontSize + "px tonyukuk";
-  } else if (selectedFont === "tonyukukel") {
-    ctx.font = fontSize + "px tonyukukel";
-  } else if (selectedFont === "tonyukukelbold") {
-    ctx.font = fontSize + "px tonyukukelbold";
-  } else if (selectedFont === "tonyukukbold") {
-    ctx.font = fontSize + "px tonyukukbold";
-  } else if (selectedFont === "irkbitig") {
-    ctx.font = fontSize + "px irkbitig";
-  } else if (selectedFont === "gokturkkurgu") {
-    ctx.font = fontSize + "px gokturkkurgu";
-  } else if (selectedFont === "gokturkkurgubold") {
-    ctx.font = fontSize + "px gokturkkurgubold";
-  } else if (selectedFont === "kultigin") {
-    ctx.font = fontSize + "px kultigin";
-  } else if (selectedFont === "kultigingolge") {
-    ctx.font = fontSize + "px kultigingolge";
-  } else if (selectedFont === "kultiginbold") {
-    ctx.font = fontSize + "px kultiginbold";
-  } else if (selectedFont === "tbldamga") {
-    ctx.font = fontSize + "px tbldamga";
-  } else if (selectedFont === "oguzbodun") {
-    ctx.font = fontSize + "px oguzbodun";
-  } else if (selectedFont === "oguzbold") {
-    ctx.font = fontSize + "px oguzbold";
-  }
+  ctx.font = fontSize + "px " + selectedFont;
 
   ctx.textAlign = "right";
   ctx.textBaseline = "top";
@@ -182,7 +133,6 @@ function updateCanvas(force = false) {
     y += fontSize * lineHeight;
   }
 
-  // Set canvas background color based on radio button selection
   canvas.style.backgroundColor = "";
   var selectedBgColor = document.querySelector(
     'input[name="bgColorRadio"]:checked'
@@ -192,34 +142,29 @@ function updateCanvas(force = false) {
   }
   canvas.style.backgroundColor = canvasColor;
 
-  // Force the canvas to update if necessary
   if (force) {
     canvas.dispatchEvent(new Event("input"));
   }
 }
 
 function downloadImage() {
-    updateCanvas();
+  updateCanvas();
 
-    // Create a new canvas with selected background color
-    var newCanvas = document.createElement('canvas');
-    newCanvas.width = canvas.width;
-    newCanvas.height = canvas.height;
-    var newCtx = newCanvas.getContext('2d');
-    newCtx.fillStyle = canvasColor;
-    newCtx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+  var newCanvas = document.createElement('canvas');
+  newCanvas.width = canvas.width;
+  newCanvas.height = canvas.height;
+  var newCtx = newCanvas.getContext('2d');
+  newCtx.fillStyle = canvasColor;
+  newCtx.fillRect(0, 0, newCanvas.width, newCanvas.height);
 
-    // Draw original canvas on the new canvas
-    newCtx.drawImage(canvas, 0, 0);
+  newCtx.drawImage(canvas, 0, 0);
 
-    // Download the new canvas as an image
-    var link = document.createElement('a');
-    link.download = 'gokturkce.png';
-    link.href = newCanvas.toDataURL();
-    link.click();
+  var link = document.createElement('a');
+  link.download = 'gokturkce.png';
+  link.href = newCanvas.toDataURL();
+  link.click();
 }
 
-// Initialize canvas on page load
 updateCanvas();
 
 var keyboardContainer = document.getElementById("keyboard");
@@ -248,13 +193,13 @@ keyboardContainer.addEventListener("click", function(event) {
   }
 });
 
-      function copyToClipboard(event) {
-        event.preventDefault();
-        var input = document.getElementById("gokturk");
-        input.select();
-        input.setSelectionRange(0, 99999); // For mobile devices
-        document.execCommand("copy");
-        console.log("Copied to clipboard: " + input.value);
-      }
+function copyToClipboard(event) {
+  event.preventDefault();
+  var input = document.getElementById("gokturk");
+  input.select();
+  input.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  console.log("Copied to clipboard: " + input.value);
+}
 
 // keyboard - canvas ends
