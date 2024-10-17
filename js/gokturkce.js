60f74fcaa77af43a906f1a1941f9c3861c6dbfc1
ismet <turkanitlari@gmail.com>
2024-10-17 14:36:25 +0800

    none

--- js/gokturkce.js
+++ js/gokturkce.js
@@ -1,6 +1,6 @@
 // Copyright (C) turkbitig.com. All Rights Reserved.
 var vowelsMap={'a':'𐰀','e':'𐰀','ı':'𐰃','i':'𐰃','o':'𐰆','u':'𐰆','ö':'𐰇','ü':'𐰇',};
-var consonantsMap={'b':['𐰉','𐰋'],'d':['𐰑','𐰓'],'g':['𐰍','𐰏'],'k':['𐰴','𐰚'],'l':['𐰞','𐰠'],'n':['𐰣','𐰤'],'r':['𐰺','𐰼'],'s':['𐰽','𐰾'],'t':['𐱃','𐱅'],'y':['𐰖','𐰘'],'ç':['𐰲','𐰲'],'ñ':['𐰭','𐰭'],'ŋ':['𐰭','𐰭'],'m':['𐰢','𐰢'],'p':['𐰯','𐰯'],'ş':['𐱁','𐱁'],'z':['𐰔','𐰔'],'c':['𐰲','𐰲'],'f':['𐰯','𐰯'],'ğ':['𐰍','𐰏'],'h':['𐰴','𐰚'],'j':['𐱁','𐱁'],'v':['𐰉','𐰋']};
+var consonantsMap={'b':['𐰉','𐰋'],'d':['𐰑','𐰓'],'g':['𐰍','𐰏'],'k':['𐰴','𐰚'],'l':['𐰞','𐰠'],'n':['𐰣','𐰤'],'r':['𐰺','𐰼'],'s':['𐰽','𐰾'],'t':['𐱃','𐱅'],'y':['𐰖','𐰘'],'ç':['𐰲','𐰲'],'ñ':['𐰭','𐰭'],'ŋ':['𐰭','𐰭'],'m':['𐰢','𐰢'],'p':['𐰯','𐰯'],'ş':['𐱁','𐱁'],'z':['𐰔','𐰔']};
 var doublesMap={'ng':'𐰭','nç':'𐰨','nd':'𐰦','nt':'𐰦','ny':'𐰪','ok':'𐰸','uk':'𐰸','ök':'𐰜','ük':'𐰜','iç':'𐰱','ık':'𐰶','ld':'𐰡'};
 var allChars = ['a', 'ı', 'o', 'u', 'e', 'i', 'ö', 'ü', 'b',  'd', 'g', 'k', 'l', 'n', 'r', 's',  't', 'y', 'ç', 'm', 'p', 'ş', 'z'];
 var backVowels = ['a', 'ı', 'o', 'u'];
@@ -8,26 +8,20 @@ var frontVowels = ['e', 'i', 'ö', 'ü'];
 
 function updateDiv() {
   var inputText = document.getElementById("latin").value.trim();
-  inputText = inputText.replace(/I/g, 'ı').replace(/İ/g, 'i');
-  inputText = inputText.replace(/F/g, 'p').replace(/f/g, 'p');
-  inputText = inputText.replace(/Ğ/g, 'g').replace(/ğ/g, 'g');
-  inputText = inputText.replace(/H/g, 'k').replace(/h/g, 'k');
-  inputText = inputText.replace(/Q/g, 'k').replace(/q/g, 'k');
-  inputText = inputText.replace(/W/g, 'b').replace(/w/g, 'b');
-  inputText = inputText.replace(/X/g, 'k').replace(/x/g, 'k');
-  inputText = inputText.replace(/V/g, 'b').replace(/v/g, 'b');
-  inputText = inputText.replace(/C/g, 'ç').replace(/c/g, 'ç');
-  inputText = inputText.replace(/J/g, 'ç').replace(/j/g, 'ç');
-  inputText = inputText.replace(/Ə/g, 'e').replace(/ə/g, 'e');
-  inputText = inputText.replace(/Ý/g, 'y').replace(/ý/g, 'y');
-  inputText = inputText.replace(/Ä/g, 'e').replace(/ä/g, 'e');
-  inputText = inputText.replace(/Ū/g, 'u').replace(/ū/g, 'u');
-  inputText = inputText.toLowerCase();
+    inputText = inputText.replace(/I/g, 'ı').replace(/İ/g, 'i');
+    inputText = inputText.replace(/[Ff]/g, 'p');
+    inputText = inputText.replace(/[Ğğ]/g, 'g');
+    inputText = inputText.replace(/[HhQqXx]/g, 'k');
+    inputText = inputText.replace(/[WwVv]/g, 'b');
+    inputText = inputText.replace(/[CcJj]/g, 'ç');
+    inputText = inputText.replace(/[ÄäƏə]/g, 'e');
+    inputText = inputText.replace(/[Ýý]/g, 'y');
+    inputText = inputText.replace(/[Ūū]/g, 'u');
+    inputText = inputText.toLowerCase();
   var processedText = processInputText(inputText);
   var gokturk = document.getElementById("gokturk");
-  gokturk.value = processedText; 
+    gokturk.value = processedText; 
 }
-
 
 function processInputText(inputText) {
   var processedText = '';
